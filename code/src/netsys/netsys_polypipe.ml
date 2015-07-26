b3ffc31dcd38482178e4a51c87d6dfc4b4ac0f23
gerd <gerd@96e5b9e0-aa12-0410-9e2e-b23d24e56262>
2015-07-26 11:35:42 +0000

    new Netsys_polysocket abstraction
    git-svn-id: https://godirepo.camlcity.org/svn/lib-ocamlnet2/branches/onet4.1@2252 96e5b9e0-aa12-0410-9e2e-b23d24e56262

--- code/src/netsys/netsys_polypipe.ml
+++ code/src/netsys/netsys_polypipe.ml
@@ -1,19 +1,27 @@
 (* $Id$ *)
 
+exception Closed
+
 type notifier =
   | NE of Netsys_posix.not_event * Unix.file_descr option
   | W32 of Netsys_win32.w32_event
 
 
-type 'a polypipe =
+type 'a pipe =
     { buffer : 'a Queue.t;
       mutable eof : bool;
       size : int;
-      non_blocking : bool;
       mutable rd_descr : notifier;
       mutable wr_descr : notifier;
       mutex : Netsys_oothr.mutex;
       mutable dead : bool;
+      mutable exn : exn option;
+    }
+
+type 'a polypipe =
+    { pipe : 'a pipe;
+      readable : bool;
+      writable : bool;
     }
 
 let is_win32 =
@@ -25,7 +33,7 @@ let create_notifier() =
   else
     NE (Netsys_posix.create_event(), None)
 
-let create n nb =
+let create_pipe n =
   let rd_descr = create_notifier() in
   let wr_descr = create_notifier() in
   ( match wr_descr with
@@ -35,169 +43,192 @@ let create n nb =
   { buffer = Queue.create();
     eof = false;
     size = n;
-    non_blocking = nb;
     rd_descr;
     wr_descr;
     mutex = !Netsys_oothr.provider # create_mutex();
     dead = false;
+    exn = None;
   }
 
+let create n =
+  let p = create_pipe n in
+  ( { pipe = p; readable = true; writable = false },
+    { pipe = p; readable = false; writable = true }
+  )
+
 let length pp =
-  pp.mutex # lock();
-  let n = Queue.length pp.buffer in
-  pp.mutex # unlock();
+  pp.pipe.mutex # lock();
+  let n = Queue.length pp.pipe.buffer in
+  pp.pipe.mutex # unlock();
   n
 
 let eof pp =
-  pp.eof
+  pp.pipe.eof
 
-let rec read pp =
-  pp.mutex # lock();
-  if pp.dead then (
-    pp.mutex # unlock();
-    failwith "Netsys_polypipe.read: pipe is closed";
+let rec read ~nonblock pp =
+  if not pp.readable then
+    raise(Unix.Unix_error(Unix.EACCES, "Netsys_polypipe.read", ""));
+  pp.pipe.mutex # lock();
+  ( match pp.pipe.exn with
+      | None -> ()
+      | Some exn ->
+          pp.pipe.mutex # unlock();
+          raise exn
   );
-  let n = Queue.length pp.buffer in
+  let n = Queue.length pp.pipe.buffer in
   if n > 0 then (
     let at_min = (n = 1) in
-    let at_max = (n >= pp.size) in
-    let msg = Queue.take pp.buffer in
+    let at_max = (n >= pp.pipe.size) in
+    let msg = Queue.take pp.pipe.buffer in
     if at_min then (
-      match pp.rd_descr with
+      match pp.pipe.rd_descr with
         | NE(ev,_) -> ignore(Netsys_posix.consume_event ev)
         | W32 ev -> Netsys_win32.reset_event ev
     );
     if at_max then (
-      match pp.wr_descr with
+      match pp.pipe.wr_descr with
         | NE(ev,_) -> Netsys_posix.set_event ev
         | W32 ev -> Netsys_win32.set_event ev
     );
-    pp.mutex # unlock();
+    pp.pipe.mutex # unlock();
     Some msg
   ) else (
-    pp.mutex # unlock();
-    if pp.eof then
+    pp.pipe.mutex # unlock();
+    if pp.pipe.eof then
       None
     else (
-      if pp.non_blocking then
+      if nonblock then
         raise(Unix.Unix_error(Unix.EAGAIN, "Netsys_polypipe.read", ""))
       else (
-        ( match pp.rd_descr with
+        ( match pp.pipe.rd_descr with
             | NE(ev,_) -> Netsys_posix.wait_event ev
             | W32 ev -> ignore(Netsys_win32.event_wait ev (-1.0))
         );
-        read pp
+        read ~nonblock pp
       )
     )
   )
 
-let rec write pp msg_opt =
-  pp.mutex # lock();
-  if pp.dead then (
-    pp.mutex # unlock();
-    failwith "Netsys_polypipe.write: pipe is closed";
+let rec write ~nonblock pp msg_opt =
+  if not pp.writable then
+    raise(Unix.Unix_error(Unix.EACCES, "Netsys_polypipe.write", ""));
+  pp.pipe.mutex # lock();
+  ( match pp.pipe.exn with
+      | None -> ()
+      | Some exn ->
+          pp.pipe.mutex # unlock();
+          raise exn
   );
   match msg_opt with
     | None ->
-        pp.eof <- true;
-        ( match pp.rd_descr with
+        pp.pipe.eof <- true;
+        ( match pp.pipe.rd_descr with
             | NE(ev,_) -> Netsys_posix.set_event ev
             | W32 ev -> Netsys_win32.set_event ev
         );
-        ( match pp.wr_descr with
+        ( match pp.pipe.wr_descr with
             | NE(ev,_) -> Netsys_posix.set_event ev;
             | W32 ev -> Netsys_win32.set_event ev
         );
-        pp.mutex # unlock();
+        pp.pipe.mutex # unlock();
     | Some msg ->
-        if pp.eof then (
-          pp.mutex # unlock();
+        if pp.pipe.eof then (
+          pp.pipe.mutex # unlock();
           raise(Unix.Unix_error(Unix.EPIPE, "Netsys_polypipe.write", ""))
         );
-        let n = Queue.length pp.buffer in
-        if n < pp.size then (
-          Queue.add msg pp.buffer;
+        let n = Queue.length pp.pipe.buffer in
+        if n < pp.pipe.size then (
+          Queue.add msg pp.pipe.buffer;
           let at_min = (n = 0) in
-          let at_max = (n+1 >= pp.size) in
+          let at_max = (n+1 >= pp.pipe.size) in
           if at_min then (
-            match pp.rd_descr with
+            match pp.pipe.rd_descr with
               | NE(ev,_) -> Netsys_posix.set_event ev
               | W32 ev -> Netsys_win32.set_event ev
           );
           if at_max then (
-            match pp.wr_descr with
+            match pp.pipe.wr_descr with
               | NE(ev,_) -> ignore(Netsys_posix.consume_event ev)
               | W32 ev -> Netsys_win32.reset_event ev
           );
-          pp.mutex # unlock();
+          pp.pipe.mutex # unlock();
         ) else (
-          pp.mutex # unlock();
-          if pp.non_blocking then
+          pp.pipe.mutex # unlock();
+          if nonblock then
             raise(Unix.Unix_error(Unix.EAGAIN, "Netsys_polypipe.write", ""));
-          ( match pp.wr_descr with
+          ( match pp.pipe.wr_descr with
               | NE(ev,_) -> Netsys_posix.wait_event ev
               | W32 ev -> ignore(Netsys_win32.event_wait ev (-1.0))
           );
-          write pp msg_opt
+          write ~nonblock pp msg_opt
         )
 
 
 let read_descr pp =
-  pp.mutex # lock();
-  if pp.dead then (
-    pp.mutex # unlock();
-    failwith "Netsys_polypipe.read_descr: pipe is closed";
+  pp.pipe.mutex # lock();
+  ( match pp.pipe.exn with
+      | None -> ()
+      | Some exn ->
+          pp.pipe.mutex # unlock();
+          raise exn
   );
-  match pp.rd_descr with
+  match pp.pipe.rd_descr with
     | NE(ev, None) ->
         let fd = Netsys_posix.get_event_fd ev in
-        pp.rd_descr <- NE(ev, Some fd);
-        pp.mutex # unlock();
+        pp.pipe.rd_descr <- NE(ev, Some fd);
+        pp.pipe.mutex # unlock();
         fd
     | NE(_, Some fd) ->
-        pp.mutex # unlock();
+        pp.pipe.mutex # unlock();
         fd
     | W32 ev ->
         let fd = Netsys_win32.event_descr ev in
-        pp.mutex # unlock();
+        pp.pipe.mutex # unlock();
         fd
 
 let write_descr pp =
-  pp.mutex # lock();
-  if pp.dead then (
-    pp.mutex # unlock();
-    failwith "Netsys_polypipe.write_descr: pipe is closed";
+  pp.pipe.mutex # lock();
+  ( match pp.pipe.exn with
+      | None -> ()
+      | Some exn ->
+          pp.pipe.mutex # unlock();
+          raise exn
   );
-  match pp.wr_descr with
+  match pp.pipe.wr_descr with
     | NE(ev, None) ->
         let fd = Netsys_posix.get_event_fd ev in
-        pp.wr_descr <- NE(ev, Some fd);
-        pp.mutex # unlock();
+        pp.pipe.wr_descr <- NE(ev, Some fd);
+        pp.pipe.mutex # unlock();
         fd
     | NE(_, Some fd) ->
-        pp.mutex # unlock();
+        pp.pipe.mutex # unlock();
         fd
     | W32 ev ->
         let fd = Netsys_win32.event_descr ev in
-        pp.mutex # unlock();
+        pp.pipe.mutex # unlock();
         fd
 
+let set_exception pp exn =
+  if pp.pipe.exn = None then
+    pp.pipe.exn <- Some exn
+
 let close pp =
-  pp.mutex # lock();
-  if not pp.dead then (
-    ( match pp.rd_descr with
+  pp.pipe.mutex # lock();
+  if not pp.pipe.dead then (
+    ( match pp.pipe.rd_descr with
         | NE(ev,_) ->
             Netsys_posix.destroy_event ev
         | W32 _ ->
             ()
     );
-    ( match pp.wr_descr with
+    ( match pp.pipe.wr_descr with
         | NE(ev,_) ->
             Netsys_posix.destroy_event ev
         | W32 _ ->
             ()
     );
-    pp.dead <- true;
+    pp.pipe.dead <- true;
+    set_exception pp Closed;
   );
-  pp.mutex # unlock()
+  pp.pipe.mutex # unlock()
 
--- code/src/netsys/netsys_polypipe.mli
+++ code/src/netsys/netsys_polypipe.mli
@@ -6,29 +6,33 @@
     to send messages of any types between threads.
  *)
 
+exception Closed
+
 type 'a polypipe
 
-val create : int -> bool -> 'a polypipe
-  (** Create a new polypipe with an internal buffer of n messages.
-      The bool says whether the pipe is non-blocking.
+val create : int -> 'a polypipe * 'a polypipe
+  (** Create a new polypipe with an internal buffer of n messages. The left
+      descriptor is open for reading, and the right is open for writing.
    *)
 
 val length : 'a polypipe -> int
   (** return the number of messages in the buffer *)
 
 val eof : 'a polypipe -> bool
-  (** whether the eof signal was sent. Note that there may still be messages
+  (** whether the eof marker was sent. Note that there may still be messages
       in the buffer
    *)
 
-val read : 'a polypipe -> 'a option
+val read : nonblock:bool -> 'a polypipe -> 'a option
   (** read a message. [None] means EOF. Possible [Unix.unix_error] codes:
        - [EAGAIN]: the pipe is non-blocking, and there is no message in the
          buffer
        - [EINTR]: a signal arrived
+
+      Raises [Closed] if the polypipe has been closed.
    *)
 
-val write : 'a polypipe -> 'a option -> unit
+val write : nonblock:bool -> 'a polypipe -> 'a option -> unit
   (** write a message, or mark the end of the stream ([None]).
       Possible [Unix.unix_error] codes:
        - [EAGAIN]: the pipe is non-blocking, and there is no message in the
@@ -37,6 +41,8 @@ val write : 'a polypipe -> 'a option -> unit
        - [EPIPE]: it was tried to send a message after sending [None].
 
       Unlike OS pipes, polypipes become writable after signaling EOF.
+
+      Raises [Closed] if the polypipe has been closed.
    *)
 
 val close : 'a polypipe -> unit
@@ -48,6 +54,8 @@ val read_descr : 'a polypipe -> Unix.file_descr
       always the same descriptor is returned.
 
       The caller has to close the descriptor after use.
+
+      Raises [Closed] if the polypipe has been closed.
    *)
 
 val write_descr : 'a polypipe -> Unix.file_descr
@@ -56,4 +64,12 @@ val write_descr : 'a polypipe -> Unix.file_descr
       If called several times, always the same descriptor is returned.
 
       The caller has to close the descriptor after use.
+
+      Raises [Closed] if the polypipe has been closed.
+   *)
+
+val set_exception : _ polypipe -> exn -> unit
+  (** Sets an exception that is returned by all further calls of
+      [read], [write], [read_descr], or [write_descr]. If an exception
+      already exists, it is not overwritten.
    *)
